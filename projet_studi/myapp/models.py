87afe3e87b61b2450eee265f018abed397b1be37
achassefiere <antoine.chassefiere@gmail.com>
2025-04-24 18:13:27 +0200

    edit models

--- projet_studi/myapp/models.py
+++ projet_studi/myapp/models.py
@@ -1,4 +1,33 @@
 from django.db import models
+from datetime import datetime, timezone
 
 # Create your models here.
-class 
+class Roles(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=100)
+    
+class Users(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+    mail = models.EmailField(unique=True)
+    key = models.CharField(max_length=255, unique=True) ## a verifier
+    password = models.CharField(max_length=255)
+    role = models.ForeignKey(Roles, on_delete=models.CASCADE)
+    
+class Offres(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=255)
+    date = models.DateField(auto_now=False, auto_now_add=False, blank=True, null=True) ## a verifier
+    time = models.TimeField(auto_now=False, auto_now_add=False, blank=True, null=True) ## a verifier
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+class Tickets(models.Model):
+    id = models.AutoField(primary_key=True)
+    key = models.CharField(max_length=255, unique=True) ## a verifier
+    concat_key = models.CharField(max_length=255, unique=True) ## a verifier
+    offre = models.ForeignKey(Offres, on_delete=models.CASCADE)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    ## a finir
+
+## add class Panier ??
--- projet_studi/projet_studi/settings.py
+++ projet_studi/projet_studi/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'projet_studi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS' : {
+            'init_command': 'SET default_storage_engine=INNODB',
+        },
+        'NAME': 'projet_studi_db',
+        'USER' : 'root',
+        'PASSWORD' : '',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
