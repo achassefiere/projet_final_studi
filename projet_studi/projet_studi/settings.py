0f507dc4aca9adf83dda853b877a2f63583d2319
achassefiere <antoine.chassefiere@gmail.com>
2025-10-19 18:41:42 +0200

    Prépare le projet pour déploiement sur Fly avec DATABASE_URL

--- projet_studi/projet_studi/settings.py
+++ projet_studi/projet_studi/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,17 +80,7 @@ WSGI_APPLICATION = 'projet_studi.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS' : {
-            'init_command': 'SET default_storage_engine=INNODB',
-        },
-        'NAME': 'projet_studi_db',
-        'USER' : 'root',
-        'PASSWORD' : '123+Aze!', 
-        'HOST' : 'localhost',
-        'PORT' : '3306',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
