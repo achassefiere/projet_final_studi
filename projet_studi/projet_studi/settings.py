472d65e96ffa1c5246336a141313f0ceb1345a05
achassefiere <antoine.chassefiere@gmail.com>
2025-10-19 20:24:50 +0200

    edit settings

--- projet_studi/projet_studi/settings.py
+++ projet_studi/projet_studi/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'projet_studi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-$env:DATABASES = {
+DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
